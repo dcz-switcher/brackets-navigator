ded5c3628d0ba20a388d9671d9b0e21e63464001
dcz-switcher <dcz.switcher@gmail.com>
2015-06-11 22:06:26 +0200

    test if language is supported for analysis and listen to editor document change (load another file, create new one …)

--- main.js
+++ main.js
@@ -8,11 +8,59 @@ define(function (require, exports, module) {
     
     var ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
         WorkspaceManager = brackets.getModule("view/WorkspaceManager"),
+        DocumentManager = brackets.getModule("document/DocumentManager"),
+        EditorManager = brackets.getModule("editor/EditorManager"),
         $toolBarButn = $('<a href="#" id="toolbar-navigator" title="navigator"></a>'),
-        $navigatorPanel = $('<div id="navigator-panel"><div id="navigator-content"><h5>NAVIGATOR</h5></div></div>'),
+        $navigatorPanel = $('<div id="navigator-panel"><div id="navigator-content"><h5>NAVIGATOR</h5><div id="navigator-tree"></div><div id="navigator-msg"></div></div></div>'),
         $content = $(".content"),
-        $mainView = $(".main-view");
+        $mainView = $(".main-view"),
+        $navigatorMsg = $navigatorPanel.find("#navigator-msg"),
+        $navigatorTree = $navigatorPanel.find("#navigator-tree"),
+        isPanelOpen = false,
+        document;
+    
         
+            
+    /**
+    * show/hide msg and write text if defined
+    */
+    var navigatorMsgDisplay = function (bool, text) {
+        if (bool) {
+            if (text === undefined) {
+                text = "";
+            }
+            $navigatorTree.hide();
+            $navigatorMsg.html(text).show();
+        } else {
+            $navigatorMsg.empty().hide();
+            $navigatorTree.show();
+        }
+    };
+    
+    /**
+    * perform analysis of the file
+    */
+    var performAnalysis = function () {
+        document = DocumentManager.getCurrentDocument();
+        if (document) {
+            if (document.language.getName() === "JavaScript") {
+//                navigatorMsgDisplay(false);
+                navigatorMsgDisplay(true, "analyse en cours ...");
+                
+                console.log(document.getText());
+            } else {
+                navigatorMsgDisplay(true, "Language non supporté");
+            }
+        } else {
+            navigatorMsgDisplay(true, "Aucun document à analyser");
+        }
+    };
+         
+            
+    var activeEditorChangeHandler = function (event, focusedEditor, lostEditor) {
+        performAnalysis();
+    };
+                     
     /**
     * init extension (only if stylesheet is loaded ... see at the bottom)
     */
@@ -26,7 +74,16 @@ define(function (require, exports, module) {
         // listen to click event on the button to show/hide navigator
         $toolBarButn.on("click", function (e) {
             $mainView.toggleClass('navigator-panel-open');
+            isPanelOpen = !isPanelOpen;
+            
+            if (isPanelOpen) {
+                performAnalysis();
+                EditorManager.on("activeEditorChange", activeEditorChangeHandler);
+            } else {
+                EditorManager.off("activeEditorChange", activeEditorChangeHandler);
+            }
         });
+        
     };
     
     /**
